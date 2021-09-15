22f45506706ea0e3db10e25d464f43bd22d10461
frojascu <frojascu@gmail.com>
2021-09-15 11:21:21 +0200

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-git clone https://github.com/frojascu/phpinfo
+git clone https://github.com/ganimedes-colomar/phpinfo
 cd phpinfo
-git ckeckout 2021-09-santander
+git checkout 2021-09-santander
 
 docker image build \
   --file ./Dockerfile \
@@ -10,14 +10,36 @@ docker image build \
   --tag local/phpinfo:test \
   ./
   
-docker netowrk create phpinfo
+## docker container run --entrypoint /bin/sh --interactive --rm --tty local/phpinfo:test
+## find / | grep bin/ | wc -l
+## which php
+
+docker network create phpinfo
 docker container run \
+  --cpus '0.1' \
   --detach \
+  --entrypoint /usr/bin/php \
+  --env author=Ganimedes \
+  --label app=phpinfo \
+  --memory 100M \
   --name phpinfo \
   --network phpinfo \
+  --publish 80:8080 \
   --read-only \
   --restart always \
   --user nobody \
-  --volume ./src/index.php:/app/index.php:ro \
+  --volume ${PWD}/src/index.php:/app/index.php:ro \
   --workdir /app/ \
-  local/phpinfo:test 
+  local/phpinfo:test \
+  -f /src/index.php \
+  -S 0.0.0.0:8080 \
+  
+## docker container ls
+## docker container ls --no-trunc
+## docker container top phpinfo
+## docker container stats phpinfo --no-stream
+## docker container logs phpinfo
+## docker container logs phpinfo --follow
+
+## docker container rm --force phpinfo
+## docker system prune --all --force --volumes
