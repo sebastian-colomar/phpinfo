0351baf90a8e896cb4b400a7dd5aab3d0fa4e7e4
Sebastian Colomar <sebastian.colomar@gmail.com>
2023-03-15 08:22:50 +0000

    docker-2/

---
+++ docker-2/README.md
@@ -0,0 +1,3 @@
+# phpinfo
+
+![CI](https://github.com/academiaonline/phpinfo/workflows/CI/badge.svg?branch=docker)
---
+++ docker-2/master.sh
@@ -0,0 +1,1 @@
+    docker swarm join --token SWMTKN-1-1so8dap0b4g257n24gne90fe8hgrne0474obxwur6c0scgs4ci-564z79zvqrn2e04fpcd3nxnjz 192.168.0.8:2377
